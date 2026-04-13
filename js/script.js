fb75dd4be46bb3b506c7f7676debadea4baba33d
exoticspam25 <114175742+leeroy1232@users.noreply.github.com>
2026-04-13 12:51:03 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -19,3 +19,8 @@ function toggleMenu() {
   const navLinks = document.getElementById("navLinks");
   navLinks.classList.toggle("show");
 }
+document.querySelectorAll("#navLinks a").forEach(link => {
+  link.addEventListener("click", () => {
+    document.getElementById("navLinks").classList.remove("show");
+  });
+});
